c0820b9708f6745847ce8b5b35c783cce8f078fe
Francisco Mendoza <56323554+devnull404@users.noreply.github.com>
2021-01-15 13:11:19 -0600

    Adding ternary operator to check users

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,9 @@
 function prev() {
     let url = "https://charal.unacar.mx/fotos/";
-    user = +document.getElementById("user").src.slice(-10, -4) -1
-    ext = ".jpg"
-    document.getElementById("user").src = url + user.toString() + ext
+    user = +document.getElementById("user").src.slice(-10, -4) -1;
+    ext = ".jpg";
+    user = checkMat(user);
+    document.getElementById("user").src = url + user.toString() + ext;
     addPlaceHolder(user);
 };
 
@@ -10,6 +11,7 @@ function next() {
     let url = "https://charal.unacar.mx/fotos/";
     user = +document.getElementById("user").src.slice(-10, -4) +1
     ext = ".jpg"
+    user = checkMat(user);
     document.getElementById("user").src = url + user.toString() + ext
     addPlaceHolder(user);
 };
@@ -28,6 +30,10 @@ function matricula() {
 function addPlaceHolder(user) {
     document.getElementById("info").value = user
 }
+
+function checkMat(mat) {
+    return mat.length < 6 ? "0"+mat: mat;
+};
 
 matri = Math.floor(Math.random()*1000)+200000
 document.getElementById("user").src += (matri).toString() + ".jpg"
